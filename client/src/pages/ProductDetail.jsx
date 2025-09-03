833e1e702d1e6a8df5c408ad8158c05d098dc850
Anshul chouhan <ansulchouhan004@gmail.com>
2025-09-03 16:44:16 +0530

    Added fifth chnages

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { api } from '../api.js'
-import '../pages/ProductDetails.css';
+import '../pages/Productdetails.css';
 
 
 // helper function to generate full image URL
